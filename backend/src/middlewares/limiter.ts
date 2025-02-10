cbea5100608cedd67b459191dde589f4c4f2b4f7
Dima <goodd214080@gmail.com>
2025-02-10 21:55:26 +0300

    Test 9

--- backend/src/middlewares/limiter.ts
+++ backend/src/middlewares/limiter.ts
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 export const limiter = rateLimit({
     windowMs: 10 * 60 * 1000,
-    max: 100,
+    max: 15,
     message: { error: 'Слишком много запросов, пожалуйста, попробуйте позже.' },
     legacyHeaders: false,
     standardHeaders: true
