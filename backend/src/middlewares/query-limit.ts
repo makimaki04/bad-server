e09c98cdca4130c9c5f51368d6dd76cab31ab4fe
Dima <goodd214080@gmail.com>
2025-02-11 10:12:42 +0300

    test 19

--- backend/src/middlewares/query-limit.ts
+++ backend/src/middlewares/query-limit.ts
@@ -2,7 +2,7 @@ import BadRequestError from "../errors/bad-request-error";
 import { MAX_LIMIT } from "../config";
 import { NextFunction, Request, Response } from "express";
 
-export function queryLimit(req: Request, res: Response, next: NextFunction) {
+export async function queryLimit (req: Request, res: Response, next: NextFunction) {
     const limit = parseInt(req.query.limit as string, 10);
     if (isNaN(limit) || limit > MAX_LIMIT) {
         req.query.limit = MAX_LIMIT.toString();
@@ -12,7 +12,7 @@ export function queryLimit(req: Request, res: Response, next: NextFunction) {
 
 const forbiddenOperators = ['$expr', '$function', '$where', '$regex'];
 
-export function validateQueryParams(req: Request, res: Response, next: NextFunction) {
+export async function validateQueryParams(req: Request, res: Response, next: NextFunction) {
     const keys = Object.keys(req.query)
 
     keys.forEach((key) => {
